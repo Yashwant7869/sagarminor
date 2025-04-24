fdc5c05a3e09ba76bc1cab37363bd321b05630c6
Sagar Pandey <151049831+sagarpandey006@users.noreply.github.com>
2025-04-24 14:08:26 +0530

    Update constant.jsx

--- client/src/utils/constant.jsx
+++ client/src/utils/constant.jsx
@@ -1,4 +1,4 @@
-export const API_END_POINT = "https://minor-civ9.onrender.com/api/v1/user";
+export const API_END_POINT = "https://sagarminor.onrender.com/api/v1/user";
 
 
 export  const options = {
