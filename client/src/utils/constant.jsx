035b78b97eaaec6a767fc4aac314331b9dedffd8
sagarpandey006 <officialsagar006@gmail.com>
2025-06-21 22:43:04 +0530

    merge conflicts

--- client/src/utils/constant.jsx
+++ client/src/utils/constant.jsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-export const API_END_POINT = "http://localhost:8080/api/v1/user";
-=======
 export const API_END_POINT = "https://sagarminor.onrender.com/api/v1/user";
->>>>>>> 9f23cc0bada2121ad6045dcf67cf935d617420ee
+
 
 
 export  const options = {
